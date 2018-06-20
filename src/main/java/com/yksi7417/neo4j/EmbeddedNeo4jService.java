f318ffabda580cb2ccad98e1d226bf52d6793c8a
yksi7417 <yatkasi@gmail.com>
2018-06-21 00:21:32 +0800

    add embedded neo4j service class.  empty

---
+++ src/main/java/com/yksi7417/neo4j/EmbeddedNeo4jService.java
@@ -0,0 +1,11 @@
+package com.yksi7417.neo4j;
+
+/**
+ * https://neo4j.com/docs/java-reference/current/tutorials-java-embedded/
+ * TODO:  add guava & guice for dependency injection and service interface?
+ */
+
+public class EmbeddedNeo4jService {
+    public void start() {}
+    public void stop() {}
+}
